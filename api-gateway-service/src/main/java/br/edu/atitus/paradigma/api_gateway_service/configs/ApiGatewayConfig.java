c08f1c18a4840f8c1883ba5feeee79fe002d8c2c
alanscheibler <132311113+alanscheibler@users.noreply.github.com>
2024-10-30 21:18:02 -0300

    Atividade avaliativa - API Gateway

--- api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
+++ api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
@@ -1,5 +1,6 @@
 package br.edu.atitus.paradigma.api_gateway_service.configs;
 
+import org.springframework.cloud.gateway.filter.factory.AddRequestHeaderGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +12,26 @@ public class ApiGatewayConfig {
 	RouteLocator getGatewayRoute(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(route -> route
+						.path("/get")
+						.filters(f-> f
+								.addRequestHeader("Usuario", "Alan Marcelo Scheibler")
+								.addRequestHeader("Teste", "ResponseHeader"))
+							.uri("http://httpbin.org"))
+				.route(route -> route
 						.path("/cambio-service/**")
+						.filters(f-> f
+								.addRequestHeader("Usuario", "Alan Marcelo Scheibler"))
 						.uri("lb://cambio-service"))
 				.route(route -> route
 					.path("/produto-service/**")
+					.filters(f-> f
+							.addRequestHeader("Usuario", "Alan Marcelo Scheibler"))
 					.uri("lb://produto-service"))
+				.route(route -> route
+						.path("/saudacao-service/**")
+						.filters(f-> f
+								.addRequestHeader("Usuario", "Alan Marcelo Scheibler"))
+						.uri("lb://saudacao-service"))
 					.build();
 	}
 }
